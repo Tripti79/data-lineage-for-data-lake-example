63a9d9d69a839fc116f8f16a7e1b108121b89f78
Tripti79 <68227435+Tripti79@users.noreply.github.com>
2024-09-05 12:12:42 +0530

    Update main.js

--- src/lineage-visual/src/main.js
+++ src/lineage-visual/src/main.js
@@ -5,7 +5,8 @@ import axios from "axios";
 
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = "https://xxx.execute-api.aws_region.amazonaws.com/dev";
+//axios.defaults.baseURL = "https://xxx.execute-api.aws_region.amazonaws.com/dev";
+axios.defaults.baseURL = "https://f1xe47nbdb.execute-api.ap-south-1.amazonaws.com/dev"; //-- FE
 
 new Vue({
   router,
